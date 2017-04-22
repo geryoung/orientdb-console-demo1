a68e560804532f3a8a4c8aad3fcfe5a445c059ab
geryoung <geryoung@163.com>
2017-04-22 16:10:16 +0800

    Update 2.addtestdata.js

--- 2.addtestdata.js
+++ 2.addtestdata.js
@@ -8,15 +8,80 @@ var db = new ODatabase({
     name: 'test2'
 });
 
+/**
+ * data:
+ * 
+ * 
+ * 
+ */
 
-db.open().then(function () {
+var personVList = [{ "name": "John", "surename": "Doe" },
+                    { "name": "John", "surename": "Doe" },
+                    { "name": "John", "surename": "Smith" },
+                    { "name": "Jenny", "surename": "Smith" },
+                    { "name": "Frank", "surename": "Bean" },
+                    { "name": "Mark", "surename": "Bean" }]
 
-    return db.insert().into('Player')
-        .set({
-            ba: 0.367,
-            r: 2246,
-            rbi: 1938
-        }).where(name = "Ty Cobb").one();
-}).then(function (player) {
-    console.log(player)
-});
+
+db.open()
+    .then(function () {
+
+        var taskList = [];
+        personVList.forEach(function (_person) {
+
+            //查找
+
+            var t = db
+                .select()
+                .from('Person')
+                .where(_person)
+                .all()
+                .then(function (select) {
+                    console.log('Hitters:', select);
+
+                    if (select && select.length != 0) {
+                        return;
+                    }
+
+
+                    //插入数据
+                    // db
+                    // .insert()
+                    // .into('Person')
+                    // .set(_person);
+
+                    db.create('VERTEX', 'Person')
+                        .set(_person)
+                        .one()
+                        .then(function (p) {
+                            console.log('Created Vertex: ' + p.name);
+                        });
+
+                });
+
+
+            taskList.push(t);
+        });
+
+
+        var task = taskList.reduce(function (preTask, currentTask) {
+            return preTask
+                .then(function (res) {
+
+                    console.log(res);
+                    return currentTask;
+                });
+        });
+
+        task
+            .then(function (res) {
+                console.log(res);
+            })
+            .catch(function (err) {
+                //
+                console.log(err);
+            })
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
