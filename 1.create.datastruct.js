84d592245159e458d93335d4a235fe7f4da03721
geryoung <geryoung@163.com>
2017-04-22 15:52:39 +0800

    Update 1.create.datastruct.js

--- 1.create.datastruct.js
+++ 1.create.datastruct.js
@@ -1,33 +1,98 @@
 var ODatabase = require('orientjs').ODatabase;
 var db = new ODatabase({
-   host:     'localhost',
-   port:     2424,
-   username: 'root',
-   password: 'hello',
-   name:     'test2'
+    host: 'localhost',
+    port: 2424,
+    username: 'root',
+    password: 'hello',
+    name: 'test2'
 });
 
 
-db.open().then(function() {
+var classList = [{
+    name: 'Person',
+    cls: 'V'
+}, {
+    name: 'Friend',
+    cls: 'E'
+}, {
+    name: 'Friend',
+    cls: 'E'
+}];
+
+
+/**
+ * class Person V
+ * class Friend E
+ */
+
+db.open().then(function () {
+
 
     //return  db.class.create('Person', 'V');
     // var F = db.class.create('Friend', 'E');
     // return db.query('SELECT FROM V LIMIT 1');
 
-    return db.class.list();
-//    .then(
-//       function(classes){
-//          console.log('There are ' 
-//          + classes.length 
-//          + ' classes in the db:',
-//          classes);
-//       }
-//    );
+    return db.class.list()
+        .then(
+        function (classes) {
+
+            var classToAddList = [];
+            classes.forEach(function (odbClsObj) {
+                classList.forEach(function (clsObj) {
+                    if (odbClsObj.name === clsObj.name &&
+                        odbClsObj.superClass === clsObj.cls) {
+                        classToAddList.push(clsObj);
+                    }
+                });
+            }, this);
+
+            return classToAddList;
+        }
+        );
 
 
-}).then(function(res){
-   //console.log(res.length);
-   db.close().then(function(){
-      console.log('closed');
-   });
+}).then(function (_classToAddList) {
+
+    var taskList = [];
+
+    if (_classToAddList && _classToAddList.forEach) {
+        _classToAddList.forEach(function (clsObj) {
+            taskList.push(db.class.create(clsObj.name, clsObj.cls));
+        })
+    }
+
+    var task = taskList.reduce(function (preTask, currentTask) {
+        return preTask
+            .then(function () {
+                console.log('aaa');
+                return currentTask;
+            })
+            .catch(function () {
+                return currentTask;
+            });
+    }, Promise.resolve());
+
+
+    return task
+        .then(function () {
+            console.log('last------------');
+        })
+        .catch(function () {
+            console.log('last------------err');
+        });
+
+
+    db.close().finally(function () {
+        console.log('closed');
+    });
+
+
 });
+
+db.on("beginQuery", function (obj) {
+    // console.log('DEBUG: ', obj);
+});
+db.on("endQuery", function (obj) {
+    // console.log("DEBUG: ", obj);
+});
+
