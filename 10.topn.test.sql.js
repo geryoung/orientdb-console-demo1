33889a0c587008e9509ac0b402ee00d6eedab8ab
geryoung <geryoung@163.com>
2017-04-27 14:10:48 +0800

    Update 10.topn.test.sql.js

--- 10.topn.test.sql.js
+++ 10.topn.test.sql.js
@@ -27,7 +27,8 @@ var sqlList = [];
 //   "CREATE VERTEX Paper SET name = 'P4'",
 //   "CREATE VERTEX Paper SET name = 'P5'",
 //   "CREATE VERTEX Paper SET name = 'P6'",
-//   "CREATE VERTEX Paper SET name = 'P7'"
+//   "CREATE VERTEX Paper SET name = 'P7'",
+//   "CREATE VERTEX Paper SET name = 'PN'"
 // ];
 
 
@@ -38,7 +39,8 @@ var sqlList = [];
 //   "CREATE VERTEX Keyword SET name = 'D'",
 //   "CREATE VERTEX Keyword SET name = 'E'",
 //   "CREATE VERTEX Keyword SET name = 'F'",
-//   "CREATE VERTEX Keyword SET name = 'G'"
+//   "CREATE VERTEX Keyword SET name = 'G'",
+//   "CREATE VERTEX Keyword SET name = 'N'"
 // ]
 
 
@@ -56,7 +58,10 @@ var sqlList = [];
 // 'create edge Contain FROM ( select from Paper where name="P6") To (select from Keyword where name="A");',
 // 'create edge Contain FROM ( select from Paper where name="P6") To (select from Keyword where name="F");',
 // 'create edge Contain FROM ( select from Paper where name="P6") To (select from Keyword where name="G");',
-// 'create edge Contain FROM ( select from Paper where name="P7") To (select from Keyword where name="A");'
+// 'create edge Contain FROM ( select from Paper where name="P7") To (select from Keyword where name="A");',
+
+// 'create edge Contain FROM ( select from Paper where name="P4") To (select from Keyword where name="N");'
+// 'create edge Contain FROM ( select from Paper where name="PN") To (select from Keyword where name="N");'
 // ];
 
 
@@ -106,15 +111,92 @@ var sqlList = [];
 // 数据查询
 ///////////////////////////////////////////////////////////////////////////////////
 var sql;
-sql = 'SELECT from Contain';
-sql = 'SELECT from Paper';
+// sql = 'SELECT from Contain';
+// sql = 'SELECT from Paper';
 
-sql = "select from Contain where inE('Contain').out('Keyword').name CONTAINS 'A'"; //n
-sql = "select from inE('Contain').out('Keyword')"; //n
-sql = "TRAVERSE out('Contain') from #34:1 WHILE $depth < 3"; //n
-sql = "select FROM (TRAVERSE in('Contain') FROM (SELECT FROM Keyword WHERE name = 'A') WHILE $depth = 0) ";
+// sql = "select from Contain where inE('Contain').out('Keyword').name CONTAINS 'A'"; //n
+// sql = "select from inE('Contain').out('Keyword')"; //n
+// sql = "TRAVERSE out('Contain') from #34:1 WHILE $depth < 3"; //n
 // sql = "select FROM (TRAVERSE both('Contain'),both('Contain') FROM (SELECT FROM Keyword WHERE name = 'A') WHILE $depth < 3 ) ";
-sql = "select FROM (TRAVERSE in('Contain') FROM (SELECT FROM Keyword WHERE name = 'A') WHILE $depth < 3 ) ";
+sql = "select FROM (TRAVERSE in('Contain'),out('Contain') FROM (SELECT FROM Keyword WHERE name = 'A') WHILE $depth < 3 ) ";
+sql = "select name FROM (TRAVERSE in('Contain'),out('Contain') FROM (SELECT FROM Keyword WHERE name = 'A') WHILE $depth < 3 )  WHERE @class='Keyword'";
+sql = "select name FROM (TRAVERSE in('Contain'),out('Contain') FROM (SELECT FROM Keyword WHERE name = 'A') WHILE $depth < 3 )  WHERE @class='Paper'";
+sql = "select FROM Contain " ; //success -> 返回 14
+
+/**
+ * Edge 搜索
+ */
+// sql = "select count(*) as ecount FROM Contain GROUP BY out ORDER BY ecount" ; //success -> 返回 7
+// sql = "select * FROM Contain WHERE in = #29:0"; 
+// sql = "select * FROM Contain WHERE out = #29:0"; 
+sql = "select * FROM Contain WHERE in = #29:0"; //success
+sql = "select * FROM Contain WHERE 'in' in ['#29:0']"; //n
+
+// sql = "select * FROM Keyword WHERE name in ['A', 'B']";
+// sql = "select * FROM Paper";
+
+
+
+// sql = "select OUT('Contain') FROM Paper WHERE name='P1'" //success
+// sql = "select OUT('Contain'),OUT('Contain') FROM Paper" // s-> 7
+// sql = "select OUT('Contain')FROM Paper" //s->7
+// sql = "select OUT('Contain').OUT('Contain') FROM Paper" //s->7
+
+
+// sql = "select OUT('Contain').OUT('Contain') FROM Paper  WHERE name='P7'" // n
+// sql = "select OUT('Contain') FROM Paper  WHERE name='P7'" //s->1
+// sql = "select OUT('Contain'),OUT('Contain') FROM Paper  WHERE name='P7'" // n
+
+
+// sql = "select OUT('Contain').name as n FROM Paper  WHERE name='P2'" //s->5[]
+
+// sql = "select in('Contain').name as n FROM Paper  WHERE n='A'" //n
+// sql = "select OUT('Contain').name as n FROM Paper  WHERE n[0]='A'" //n
+
+// sql = "select in('Contain').name as n FROM Paper  WHERE name='P2'" //n
+
+
+/**
+ * 
+ */
+// sql = "select OUT('Contain').in('Contain').name as n FROM Paper  WHERE name='P2'" //y
+// sql = "select OUT('Contain').name as n FROM Paper  WHERE name='P2'" //y
+// sql = "select in('Contain').name as n FROM Keyword  WHERE name in ['A', 'B'] " //y
+
+
+/**
+ * 相关关键词，潜在关键词
+ */
+// sql = 'MATCH {class:Keyword, as:keyword, where: (name = "A")}.in("Contain").out("Contain")' +
+//   '{as: kes} RETURN keyword, kes'; //s-7
+// sql = 'MATCH {class:Keyword, as:keyword, where: (name = "A")}.in("Contain").out("Contain").in("Contain").out("Contain")' +
+//   '{as: kes} RETURN keyword, kes'; //s-8 多一个 关键词 N
+
+// sql = "select in('Contain').out('Contain').name as n FROM Keyword GROUP BY n UNWIND n WHERE name='A' " //n
+// sql = "select in('Contain').out('Contain').name as n  FROM Keyword WHERE name='A' " //n
+
+
+var sql2 = 'MATCH {class:Person, as:person, where: (name= "John" AND surename="Doe")}.both("Friend")' +
+  '{as: friendOfFriend} RETURN person, friendOfFriend'; //
+
+/**
+ * 最短路径
+ */
+// sql = "select shortestPath(#25:0, #25:1)"; //success
+// sql = "select shortestPath(#25:0, #25:1, null, 'Contain')"; //success
+// sql = "select shortestPath(#25:0, #25:1, null, 'Contain', {'maxDepth': 0})";  //success
+// sql = "select shortestPath((select from Keyword where name='A'), (select from Keyword where name='B'))"; //success
+
+// sql = "CREATE VERTEX Paper SET name = 'PN'";
+// sql = "CREATE VERTEX Keyword SET name = 'N'";
+// sql = 'create edge Contain FROM ( select from Paper where name="P4") To (select from Keyword where name="N");';
+// sql = 'create edge Contain FROM ( select from Paper where name="PN") To (select from Keyword where name="N");';
+
+
+
+//sql = "select name "
+//sql = "select FROM Contain ";
+
 
 //sql = 'SELECT from Keyword';
 
